f8611ec30c6aab49bf502f4bd6bba531d515d878
gigib <glendaandrews77@gmail.com>
2023-10-14 09:52:51 -0700

    Task_mgr

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,50 @@
-from flask import Flask # from the flask module import the Flask class
+from flask import Flask, request
+from app.database import task
 
-#OOPP -> Object Oriented Paradigm
-app = Flask (__name__)  # create an instance of Flask (an object)
-                        # variables that belong to a class
-                        #are called (class) attributes.
-                        #Functions that belong to a class
-                        #are called "methods",
-@app.get("/")           # Flask decorator
-def index():            # View function
-    me = {
-        "first_name": "Glenda",
-        "last_name": "GPL",
-        "hobbies": "DIY stuff",
-        "is_active": True
+app = Flask(__name__)
+
+
+@app.get("/tasks")
+def get_all_tasks():
+    tasks = task.scan()
+    out = {
+        "task": tasks,
+        "ok": True,
     }
-    return me            # return statement (JSON)
+    return out
+@app.get("/tasks/{pk}")
+def get_task_by_id(pk: int):
+    single_task = task.select_by_id(pk)
+    if not single_task:
+        return {"ok": False, "message": "Not found"}, 404
+    else:   
+        out = {
+            "ok": True,
+            "task": single_task
+        }
+    return out
+
+
+@app.post("/tasks")
+def create_task():
+    task_data = request.json
+    task.insert(task_data)
+    out ={
+        "ok": True,
+        "message": "Success"
+    }
+ 
+    return"", 201
+
+@app.put("/tasks/<int:pk>")
+def update_task(pk):
+    task_data = request.json
+    task.update_by_id(pk, task_data)
+    return"",204
+
+@app.delete("/tasks/int:pk>")
+def delete_task(pk):
+    task.delete(pk)
+    return"", 204
+
+
